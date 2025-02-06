53e6f41137545666287d107c7009b8269d1e9e00
mariaalvarino <lucia.alvarino@udea.edu.co>
2025-02-06 00:04:53 -0500

    DIAGRAMA

--- build-versionfinal-Desktop_Qt_6_6_2_MinGW_64_bit-Release/release/moc_mainmenu.cpp
+++ build-versionfinal-Desktop_Qt_6_6_2_MinGW_64_bit-Release/release/moc_mainmenu.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../../Downloads/versionfinalcorregidalevel1/versionfinal/mainmenu.h"
+#include "../../versionfinal/mainmenu.h"
 #include <QtCore/qmetatype.h>
 
 #if __has_include(<QtCore/qtmochelpers.h>)
